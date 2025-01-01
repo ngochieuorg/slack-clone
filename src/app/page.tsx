3cc3a4b94c59968dd05eb02f3b50310eaeaacee0
ngochieu276 <ngochieu2761998@gmail.com>
2025-01-01 09:46:41 +0700

    auth form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button";
+import AuthScreen from "@/features/auth/components/auth-screen";
 
 export default function Home() {
-  return <Button variant={"slack"}>Helo</Button>;
+  return <AuthScreen />;
 }
---
+++ src/features/auth/components/auth-screen.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React, { useState } from "react";
+import { SignInFlow } from "../type";
+import SignInCard from "./sign-in-card";
+import SignUpCard from "./sign-up-card";
+
+const AuthScreen = () => {
+  const [state, setState] = useState<SignInFlow>("signin");
+  return (
+    <div className="h-full flex items-center justify-center bg-[#5C3B58]">
+      <div className="md:h-auto md:w-[420px]">
+        {state === "signin" ? (
+          <SignInCard setState={setState} />
+        ) : (
+          <SignUpCard setState={setState} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AuthScreen;

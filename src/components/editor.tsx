6742375837a0c9d9f1e5aa48510dec4fe4f9ec33
ngochieu276 <ngochieu2761998@gmail.com>
2025-01-13 09:38:08 +0700

    emoji

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -8,6 +8,7 @@ import { MdSend } from "react-icons/md";
 import { ImageIcon, Smile } from "lucide-react";
 import Hint from "./hint";
 import { cn } from "@/lib/utils";
+import EmojiPopover from "./emoji-popover";
 
 type EditorValue = {
   image: File | null;
@@ -125,6 +126,12 @@ const Editor = ({
     }
   };
 
+  const emojiSelect = (emoji: any) => {
+    const quill = quillRef.current;
+
+    quill?.insertText(quill?.getSelection()?.index || 0, emoji.native);
+  };
+
   const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
   return (
@@ -142,16 +149,11 @@ const Editor = ({
               <PiTextAa className="size-4" />
             </Button>
           </Hint>
-          <Hint label="Emoji">
-            <Button
-              className=""
-              disabled={disabled}
-              variant={"ghost"}
-              onClick={() => {}}
-            >
+          <EmojiPopover onEmojiSelect={emojiSelect}>
+            <Button className="" disabled={disabled} variant={"ghost"}>
               <Smile className="size-4" />
             </Button>
-          </Hint>
+          </EmojiPopover>
           {variant === "create" && (
             <Hint label="Image">
               <Button
@@ -201,11 +203,18 @@ const Editor = ({
           )}
         </div>
       </div>
-      <div className="p-2 text-[10px] text-muted-foreground flex justify-end">
-        <p>
-          <strong>Shift + Rerturn</strong> to add new line
-        </p>
-      </div>
+      {variant === "create" && (
+        <div
+          className={cn(
+            "p-2 text-[10px] text-muted-foreground flex justify-end opacity-0 transition",
+            !isEmpty && "opacity-100"
+          )}
+        >
+          <p>
+            <strong>Shift + Rerturn</strong> to add new line
+          </p>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/components/emoji-popover.tsx
@@ -0,0 +1,59 @@
+import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
+
+import { useState } from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+
+interface EmojiPopoverProp {
+  children: React.ReactNode;
+  hint?: string;
+  onEmojiSelect: (emoji: any) => void;
+}
+
+const EmojiPopover = ({
+  children,
+  hint = "Emoji",
+  onEmojiSelect,
+}: EmojiPopoverProp) => {
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const [tooltipOpen, setTooltipOpen] = useState(false);
+
+  const onSelect = (emoji: any) => {
+    onEmojiSelect(emoji);
+    setPopoverOpen(false);
+
+    setTimeout(() => {
+      setTooltipOpen(false);
+    }, 500);
+  };
+
+  return (
+    <TooltipProvider>
+      <Popover open={popoverOpen} onOpenChange={setPopoverOpen}>
+        <Tooltip
+          open={tooltipOpen}
+          onOpenChange={setTooltipOpen}
+          delayDuration={50}
+        >
+          <PopoverTrigger asChild>
+            <TooltipTrigger asChild>{children}</TooltipTrigger>
+          </PopoverTrigger>
+          <TooltipContent className="bg-black text-white border border-white/5">
+            <p className="font-medium text-xs">{hint}</p>
+          </TooltipContent>
+        </Tooltip>
+        <PopoverContent className="p-0 w-full border-none shadow-none">
+          <Picker data={data} onEmojiSelect={onSelect}></Picker>
+        </PopoverContent>
+      </Popover>
+    </TooltipProvider>
+  );
+};
+
+export default EmojiPopover;
